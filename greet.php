f5fa030d06dcb583f7dd012f51a1efef4e3683da
unknown <ilcerchiofotoeventi@gmail.com>
2026-02-10 13:19:24 +0100

    Aggiungere file greet.php iniziale

---
+++ greet.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Ritorna un saluto personalizzato.
+ */
+function greet(string $name): string
+{
+    return "Ciao, {$name}!";
+}
+
+// Comando di esecuzione: php greet.php [nome]
+// Esempio: php greet.php Mario
+$name = $argv[1] ?? "Mondo";
+echo greet($name) . PHP_EOL;
